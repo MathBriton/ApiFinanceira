bf1e1bc78a978b44e01af7036eaf1605b7236434
MathBriton <mathhbrito@gmail.com>
2025-02-21 21:26:34 -0300

    pre-release

--- src/Fina.Web/obj/Debug/net9.0/Fina.Web.AssemblyInfo.cs
+++ src/Fina.Web/obj/Debug/net9.0/Fina.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fina.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3aba20be8d6419a3f4a1e88c1fbbffdddac6689e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82b0fb285772695b574eb80f1382ef5de09e80d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fina.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fina.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
